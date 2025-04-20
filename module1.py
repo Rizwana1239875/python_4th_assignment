19c2de03f8aca9dcecd926271d5328113c14bc6d
Rizwana1239875 <142738234+Rizwana1239875@users.noreply.github.com>
2025-04-21 01:14:49 +0500

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,11 @@
+# Module in python:
+# A module is a file containing python definition and statements.
+# provide proper organization,code reuseability,namespace management andencapsulation.
+
+  def sum(a,b):
+    c=a+b
+    return c
+
+def mul(a,b):
+    c=a*b
+    return c
