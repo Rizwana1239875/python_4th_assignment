38e13d92a1b1fae78827d13f25e0c11ca81fcc56
Rizwana1239875 <142738234+Rizwana1239875@users.noreply.github.com>
2025-04-21 01:13:50 +0500

    Add files via upload

---
+++ module.py
@@ -0,0 +1,22 @@
+# as function
+
+# import module1
+# print(module1.sum(10,20))
+
+# as alias
+
+# import module1 as m
+# print(m.sum(10,20))
+# print(m.mul(10,20))
+
+
+# other way to call module:
+#from module1 import sum
+#print(sum(10,20))
+
+#For all function call we use *.
+from module1 import *
+print(sum(10,20))
+print(mul(10,20))
+
+
