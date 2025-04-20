00a061a1afa942df1f80b55107b3f6405b661293
Rizwana1239875 <142738234+Rizwana1239875@users.noreply.github.com>
2025-04-21 01:20:31 +0500

    Add files via upload

---
+++ exceptionhandling.py
@@ -0,0 +1,43 @@
+# Exception handling inpython:(we can handle these errors) 
+# 1 Zero Division Error.
+a=10
+print(a/0)
+
+# 2 Name error.
+# print(b)
+
+# 3 Type Error.
+# print(10+"20")
+
+# 4 Value Error.
+# print(int("abc"))
+ #print(int("10.5"))
+
+
+# 5 Index Error.
+ #list1=[1,2,3,4,]
+ #print(list1[5])
+
+# Key Error.
+ #dict1={"name":"Asad","age":20}
+ #print(dict1{"address"})
+
+# 7 Module Not Found Error.
+ #import calculator
+ #calculator.addition(10,20)
+
+
+# 8 Import Error.
+ #from calculator import addition
+ #addition(10,20)
+
+# 9 Attribute Error.
+ #class Student:
+  #def_int_(self,name,age):
+ #self.name=name
+ #self.age=age
+ #def display(self):
+
+
+
+
